95ff8e185f8174d537d3a1227d0cb0f19aebce7f
Vitalii Slobodianyk <vitalii.slobodianyk@oicr.on.ca>
2015-09-17 15:04:17 -0400

    Added IdClient factory

---
+++ dcc-id-client/src/main/java/org/icgc/dcc/id/client/core/IdClientFactory.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2015 The Ontario Institute for Cancer Research. All rights reserved.                             
+ *                                                                                                               
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with                                  
+ * this program. If not, see <http://www.gnu.org/licenses/>.                                                     
+ *                                                                                                               
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.icgc.dcc.id.client.core;
+
+import java.io.Serializable;
+
+import org.icgc.dcc.id.client.http.HttpIdClient;
+import org.icgc.dcc.id.client.util.HashIdClient;
+
+public class IdClientFactory implements Serializable {
+
+  private static final String HTTP_ID_CLIENT_CLASSNAME = HttpIdClient.class.getName();
+
+  private final String idClassName;
+  private final String serviceUri;
+  private final String releaseName;
+  private final String authToken;
+
+  /**
+   * Creates {@link HashIdClient}
+   */
+  public IdClientFactory(String serviceUri, String releaseName) {
+    this.idClassName = HashIdClient.class.getName();
+    this.serviceUri = serviceUri;
+    this.releaseName = releaseName;
+    this.authToken = null;
+  }
+
+  /**
+   * Creates {@link HttpIdClient}
+   */
+  public IdClientFactory(String serviceUri, String releaseName, String authToken) {
+    this.idClassName = HTTP_ID_CLIENT_CLASSNAME;
+    this.serviceUri = serviceUri;
+    this.releaseName = releaseName;
+    this.authToken = authToken;
+  }
+
+  public IdClient create() {
+    if (HTTP_ID_CLIENT_CLASSNAME.equals(idClassName)) {
+      return new HttpIdClient(serviceUri, releaseName, authToken);
+    } else {
+      return new HashIdClient(serviceUri, releaseName);
+    }
+  }
+
+}
