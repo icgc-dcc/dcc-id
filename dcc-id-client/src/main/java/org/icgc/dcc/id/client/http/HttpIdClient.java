461557ca6a9c92968143fa9e5a27ca7e7cd55569
rtisma <rtisma@gmail.com>
2017-11-27 15:14:32 -0500

    moved analysisId validation to server side

--- dcc-id-client/src/main/java/org/icgc/dcc/id/client/http/HttpIdClient.java
+++ dcc-id-client/src/main/java/org/icgc/dcc/id/client/http/HttpIdClient.java
@@ -49,9 +49,7 @@ import static org.icgc.dcc.id.core.Prefixes.FILE_ID_PREFIX;
 import static org.icgc.dcc.id.core.Prefixes.MUTATION_ID_PREFIX;
 import static org.icgc.dcc.id.core.Prefixes.SAMPLE_ID_PREFIX;
 import static org.icgc.dcc.id.core.Prefixes.SPECIMEN_ID_PREFIX;
-import static org.icgc.dcc.id.util.Ids.validateAnalysisId;
 import static org.icgc.dcc.id.util.Ids.validateId;
-import static org.icgc.dcc.id.util.Ids.validateUuid;
 
 @Slf4j
 public class HttpIdClient implements IdClient {
@@ -152,7 +150,6 @@ public class HttpIdClient implements IdClient {
   }
 
   private Optional<String> getAnalysisId(String submittedAnalysisId, boolean create) {
-    validateAnalysisId(submittedAnalysisId);
     val request = resource
         .path(ANALYSIS_ID_PATH)
         .queryParam("submittedAnalysisId", submittedAnalysisId)
@@ -163,9 +160,7 @@ public class HttpIdClient implements IdClient {
   private Optional<String> getRandomAnalysisId() {
     val request = resource
         .path(ANALYSIS_ID_PATH);
-    val id = getResponse(request);
-    validateUuid(id);
-    return id;
+    return getResponse(request);
   }
 
   @Override
